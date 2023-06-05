93b229151eaa2ceccf710278ed1bd0c1f2c4d239
arvin018 <arvin.aditama@gmail.com>
2023-06-05 16:51:47 +0700

    update documetn

--- src/views/documentation/index.vue
+++ src/views/documentation/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div >
+  <div>
     <h1 style="color: red;">Button</h1>
 
     <!-- Elemen dengan tampilan warna orange -->
@@ -9,132 +9,124 @@
     <el-button type="primary" class="black-element">Black Button</el-button>
 
     <h1>Tabel</h1>
+    <div style="margin: 20px;">
+      <el-table :data="tableData" style="width: 100%" :row-class-name="tableRowClassName">
+        <el-table-column fixed prop="date" label="Date" width="150" />
+        <el-table-column prop="name" label="Name" width="120" />
+        <el-table-column prop="state" label="State" width="120" />
+        <el-table-column prop="city" label="City" width="120" />
+        <el-table-column prop="address" label="Address" width="120" />
+        <el-table-column prop="zip" label="Zip" width="120" />
 
-    <el-table :data="tableData"  style="width: 100%" :row-class-name="tableRowClassName" >
-      <el-table-column fixed prop="date" label="Date" width="150">
-      </el-table-column>
-      <el-table-column prop="name" label="Name" width="120" > </el-table-column>
-      <el-table-column prop="state" label="State" width="120">
-      </el-table-column>
-      <el-table-column prop="city" label="City" width="120"> </el-table-column>
-      <el-table-column prop="address" label="Address" width="120">
-      </el-table-column>
-      <el-table-column prop="zip" label="Zip" width="120"> </el-table-column>
+        <el-table-column prop="zip" label="Zip" width="120" />
 
-      <el-table-column prop="zip" label="Zip" width="120"> </el-table-column>
+        <el-table-column prop="zip" label="Zip" width="120" />
 
-      <el-table-column prop="zip" label="Zip" width="120"> </el-table-column>
+        <el-table-column prop="zip" label="Zip" width="120" />
 
-      <el-table-column prop="zip" label="Zip" width="120"> </el-table-column>
+        <el-table-column prop="zip" label="Zip" width="120" />
 
-      <el-table-column prop="zip" label="Zip" width="120"> </el-table-column>
+        <el-table-column prop="zip" label="Zip" width="120" />
 
-      <el-table-column prop="zip" label="Zip" width="120"> </el-table-column>
-      
-      <el-table-column prop="zip" label="Zip" width="120"> </el-table-column>
+        <el-table-column prop="zip" label="Zip" width="120" />
 
-      <el-table-column fixed="right" label="Operations" width="120">
-        <template slot-scope="scope">
-          <el-button type="primary" icon="el-icon-edit" circle></el-button>
-          <el-button
-            @click.native.prevent="deleteRow(scope.$index, tableData)"
-            type="danger"
-            icon="el-icon-delete"
-            circle
-            size="small"
-          >
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+        <el-table-column fixed="right" label="Operations" width="120">
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" circle />
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              circle
+              size="small"
+              @click.native.prevent="deleteRow(scope.$index, tableData)"
+            />
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script>
-import $ from "jquery";
-import "datatables.net";
 
 export default {
-  name: "Documentation",
-  methods: {
-    deleteRow(index, rows) {
-      rows.splice(index, 1);
-    },
-    tableRowClassName({row, rowIndex}) {
-        if (rowIndex === 1) {
-          return 'warning-row';
-        } else if (rowIndex === 3) {
-          return 'success-row';
-        }
-        return '';
-      }
-  },
+  name: 'Documentation',
   data() {
     return {
       tableData: [
         {
-          date: "2016-05-03",
-          name: "Tom",
-          state: "California",
-          city: "Los Angeles",
-          address: "No. 189, Grove St, Los Angeles",
-          zip: "CA 90036",
+          date: '2016-05-03',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
         },
         {
-          date: "2016-05-02",
-          name: "Tom",
-          state: "California",
-          city: "Los Angeles",
-          address: "No. 189, Grove St, Los Angeles",
-          zip: "CA 90036",
+          date: '2016-05-02',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
         },
         {
-          date: "2016-05-04",
-          name: "Tom",
-          state: "California",
-          city: "Los Angeles",
-          address: "No. 189, Grove St, Los Angeles",
-          zip: "CA 90036",
+          date: '2016-05-04',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
         },
         {
-          date: "2016-05-01",
-          name: "Tom",
-          state: "California",
-          city: "Los Angeles",
-          address: "No. 189, Grove St, Los Angeles",
-          zip: "CA 90036",
+          date: '2016-05-01',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
         },
         {
-          date: "2016-05-08",
-          name: "Tom",
-          state: "California",
-          city: "Los Angeles",
-          address: "No. 189, Grove St, Los Angeles",
-          zip: "CA 90036",
+          date: '2016-05-08',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
         },
         {
-          date: "2016-05-06",
-          name: "Tom",
-          state: "California",
-          city: "Los Angeles",
-          address: "No. 189, Grove St, Los Angeles",
-          zip: "CA 90036",
+          date: '2016-05-06',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
         },
         {
-          date: "2016-05-07",
-          name: "Tom",
-          state: "California",
-          city: "Los Angeles",
-          address: "No. 189, Grove St, Los Angeles",
-          zip: "CA 90036",
-        },
-      ],
-    };
+          date: '2016-05-07',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
+        }
+      ]
+    }
   },
-  mounted() {
-    $(this.$el).DataTable();
-  },
-};
+  methods: {
+    deleteRow(index, rows) {
+      rows.splice(index, 1)
+    },
+    tableRowClassName({ row, rowIndex }) {
+      if (rowIndex === 1) {
+        return 'warning-row'
+      } else if (rowIndex === 3) {
+        return 'success-row'
+      }
+      return ''
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
